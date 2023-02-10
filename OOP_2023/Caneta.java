0a9dbd99b2d1512b015d2403a45f0ead2f10bba7
Fabio Tiburi <fabio.tiburi@gmail.com>
2023-02-09 22:29:21 -0300

    final

--- OOP_2023/Caneta.java
+++ OOP_2023/Caneta.java
@@ -5,24 +5,34 @@ public class Caneta {
     int carga;
     boolean tampada;
 
-    /**
-     * 
-     */
     void status() {
+        System.out.println("Modelo:" + this.modelo);
         System.out.println("Uma caneta " + this.cor);
-        System.out.println(" está " + this.tampada);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga:" + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+
 
     }
     void rabiscar(){
+        if (this.tampada == true){
+            System.out.println("ERRO!  Não posso rabiscar");}
+          else {
+            System.out.println("Estou rabiscando");
+          }  
+        }
         
-    }
+    
 
     void tampar(){
+        this.tampada = true; //referencia ao proprio objeto que chamou. Neste caso this equivale a ca
+
 
 
     }
 
     void destampar() {
+        this.tampada =false;
 
 
     }
