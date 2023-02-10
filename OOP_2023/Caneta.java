a0e334b4e4fcfafb5c72b92235767c9a56498028
Fabio Tiburi <fabio.tiburi@gmail.com>
2023-02-09 22:09:05 -0300

    start

---
+++ OOP_2023/Caneta.java
@@ -0,0 +1,29 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    /**
+     * 
+     */
+    void status() {
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println(" está " + this.tampada);
+
+    }
+    void rabiscar(){
+        
+    }
+
+    void tampar(){
+
+
+    }
+
+    void destampar() {
+
+
+    }
+}
--- OOP_2023/Oop_1.java
+++ OOP_2023/Oop_1.java
@@ -1,6 +1,12 @@
 public class Oop_1{
 public static void main (String[] args){
+ Caneta c1 = new Caneta();
+ c1.cor = "Azul";
+ c1.ponta = 0.5f;
+ c1.tampada = false;
+ c1.status();
 
-    
+
 }
+
 }
