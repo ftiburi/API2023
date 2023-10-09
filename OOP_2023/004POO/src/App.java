5b724103c5c95197afeac64dd3d79259fa2533cb
Fabio Tiburi <fabio.tiburi@gmail.com>
2023-10-08 23:15:36 -0300

    M10

---
+++ OOP_2023/004POO/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       Caneta c1 = new Caneta();
+        c1.setModelo("Bic");
+        c1.setPonta(0.5f);
+        c1.status();
+    }
+}
